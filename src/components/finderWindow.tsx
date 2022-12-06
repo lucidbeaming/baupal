38f147c522800f7f4423ff85164fb4b9eeba6bd5
Joshua Curry <zenphoto@gmail.com>
2022-12-07 00:10:49 +0100

    Created functions and props for main window display

--- src/components/finderWindow.tsx
+++ src/components/finderWindow.tsx
@@ -28,14 +28,49 @@ const initialValues = [{
 ]
 
 function FinderWindow() {
-  const [folders, setFolders] = useState(null)
+  const [folders, setFolders] = useState(initialValues)
+  const [activeFolder, setActiveFolder] = useState(null)
+
+  useEffect(() => {
+    console.log(activeFolder)
+  }, [activeFolder])
 
   function removeItem(obj: any, id: string) {
-
+    let collection = _.cloneDeep(obj)
+    _.eachDeep(collection, (value, key, parentObj, ctx) => {
+      if (value === id) {
+        const itemParents: any = ctx?.parents
+        const parentArray: any = itemParents[itemParents.length - 2]
+        let itemArray: any = []
+        let itemPath: string = ''
+        if (parentArray.depth === 0) {
+          itemArray = collection
+          _.remove(itemArray, { id: id })
+          collection = itemArray
+        } else {
+          itemArray = _.get(collection, parentArray.path)
+          itemPath = parentArray.path
+          _.remove(itemArray, { id: id })
+          _.set(collection, itemPath, itemArray)
+        }
+        return collection
+      }
+    })
+    return collection
   }
 
-  function addItem(obj: any, id: string) {
-
+  function addItem(obj: any, parent: string, id: string) {
+    let collection = _.cloneDeep(obj)
+    const emptyChildArray: any = []
+    _.eachDeep(collection, (value, key, parentObj, ctx) => {
+      if (parentObj.id === parent && parentObj.children && key === 'children') {
+        const childPath: any = ctx.path
+        let childArray = parentObj.children
+        childArray = childArray.concat([{ id: id, name: 'folder', children: [...emptyChildArray] }])
+        _.set(collection, childPath, childArray)
+      }
+    })
+    return collection
   }
 
   function handleRemove(id: string) {
@@ -44,6 +79,7 @@ function FinderWindow() {
   }
 
   function handleAdd(parent: string) {
+    const id: string = uuid()
     const updatedFolderList = addItem(folders, parent, id)
     setFolders(updatedFolderList)
   }
@@ -51,7 +87,7 @@ function FinderWindow() {
   return (
     <div className="finder-window">
       <Row>
-        <FolderContents />
+        <FolderContents items={folders} allFolders={folders} onAdd={handleAdd} onRemove={handleRemove} active={activeFolder} setActive={setActiveFolder}/>
       </Row>
     </div>
   )
